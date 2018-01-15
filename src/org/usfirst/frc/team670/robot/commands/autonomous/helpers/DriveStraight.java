521c724cef6f0902ef98871e4ac4299722a713a2
Shaylan Dias <shaylandias@gmail.com>
2018-01-15 15:00:28 -0800

    Update DriveStraight.java

--- src/org/usfirst/frc/team670/robot/commands/autonomous/helpers/DriveStraight.java
+++ src/org/usfirst/frc/team670/robot/commands/autonomous/helpers/DriveStraight.java
@@ -1,67 +1,79 @@
 package org.usfirst.frc.team670.robot.commands.autonomous.helpers;
 
 import org.usfirst.frc.team670.robot.Robot;
-import org.usfirst.frc.team670.robot.SensorThread;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
+ * 
+ * Uses a PID control loop plus the navX getDisplacement to move a given distance in feet
+ * 
+ * @author shayl
  *
  */
-public class DriveStraight extends Command {
+public class DriveStraight extends Command{
 
-	private char direction;
-	private double inches, start, finalInches;
+	private double distance, finalDistance;
+    private double integral, derivative, previous_error = 0;
+    private double P, I, D = 0.1;
+
+	public DriveStraight(double feet) {
+		// Use requires() here to declare subsystem dependencies
+		distance = feet;
+		requires(Robot.driveBase);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		finalDistance = getDisplacement() + distance;
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double speed;
+		double error = finalDistance - getDisplacement(); // Error = Target - Actual
+        this.integral += (error*.02); // Integral is increased by the error*time (which is .02 seconds using normal IterativeRobot)
+        derivative = (error - this.previous_error) / .02;
+        speed = P*error + I*this.integral + D*derivative;
+        previous_error = error;
+		
+		if(distance > 0)
+			Robot.driveBase.drive(speed, speed);
+		else if(distance < 0) 
+			Robot.driveBase.drive(-speed, -speed);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+
+		if(Math.abs(finalDistance - getDisplacement()) < 0.5)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.driveBase.drive(0, 0);
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		Robot.driveBase.drive(0, 0);
+
+	}
 	
-    public DriveStraight(double inches, char direction) {
-        // Use requires() here to declare subsystem dependencies
-    	this.direction = direction;
-    	this.inches = inches;
-    	Robot.sensors.reset();
-    	start = Robot.sensors.getDisplacementY();
-    	this.finalInches = start + inches;
-        requires(Robot.driveBase);
-    }
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	double percent = (finalInches - Robot.sensors.getDisplacementY())/inches;
-    	double speed = (0.8) - ((2.8)*(Math.pow(percent-0.5, 2)));
-    	
-    	if(direction == 'f')
-    		Robot.driveBase.drive(speed, speed);
-    	else if(direction == 'b') 
-    		Robot.driveBase.drive(-speed, -speed);
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-    	double percent = (finalInches - Robot.sensors.getDisplacementY())/inches;
-
-    	if(percent <= 0.025)
-    	{
-    		Robot.driveBase.drive(0, 0);
-    		return true;
-    	}
-    	else 
-    	{
-    		return false;
-    	}
-        
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.driveBase.drive(0, 0);
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	/**
+	 * Gets Displacement in feet
+	 */
+	private double getDisplacement() {
+		return Robot.sensors.getDisplacementY() * 3.28084;
+	}
+	
 }
