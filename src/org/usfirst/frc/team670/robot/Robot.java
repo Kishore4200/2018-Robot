0da65d1796756e666ea2a309620a76c889392352
Vikram Sharma <vsharma8363@gmail.com>
2018-01-16 15:59:55 -0800

    Binary changes

--- src/org/usfirst/frc/team670/robot/Robot.java
+++ src/org/usfirst/frc/team670/robot/Robot.java
@@ -17,7 +17,7 @@ import org.usfirst.frc.team670.robot.commands.autonomous.Auto_Center;
 import org.usfirst.frc.team670.robot.commands.autonomous.Auto_Left;
 import org.usfirst.frc.team670.robot.commands.autonomous.Auto_Right;
 import org.usfirst.frc.team670.robot.commands.autonomous.CancelCommand;
-import org.usfirst.frc.team670.robot.commands.components.DriveDistance_NavX;
+import org.usfirst.frc.team670.robot.commands.components.NavX_DriveDistance;
 import org.usfirst.frc.team670.robot.commands.components.NavX_Pivot;
 import org.usfirst.frc.team670.robot.commands.components.Vision_LocatePowerUp;
 import org.usfirst.frc.team670.robot.commands.joysticks.Joystick_Elevator;
@@ -82,7 +82,7 @@ public class Robot extends TimedRobot {
 			m_chooser.addObject("Turn 180 degrees", new NavX_Pivot(180));
 
 			m_chooser.addObject("Turn Right 60 degrees", new NavX_Pivot(60));
-			m_chooser.addObject("Drive 1 Foot", new DriveDistance_NavX(1));
+			m_chooser.addObject("Drive 1 Foot", new NavX_DriveDistance(1));
 
 
 		//	m_chooser.addObject("Center Switch Auto", new Auto_Center());
